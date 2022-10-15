3cd1785695d1087a79b04b8fa9e752d32554456c
Art <art-75@yandex.ru>
2022-10-15 20:19:28 +0300

    Seminar002_Task3_Project

--- Seminar002_Task3/Program.cs
+++ Seminar002_Task3/Program.cs
@@ -9,3 +9,4 @@ if (number == 4) Console.Write("Четверг");
 if (number == 5) Console.Write("Пятница");
 if (number == 6) Console.Write("Суббота");
 if (number == 7) Console.Write("Воскресенье");
+if ((number > 7) || (number < 1)) Console.Write("Проверьте, правильно ли введена цифра");
